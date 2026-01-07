dc5335e3706eefdf7bfda46477202ac0bacc56c3
Kunal <pankunal3@gmail.com>
2026-01-08 00:30:39 +0530

    Create DAY 6.java

---
+++ DAY 6.java
@@ -0,0 +1,38 @@
+//Problem 977 Leetcode
+
+class Solution {
+    public int[] mergesortedarray(int[] a, int[] b) {
+        
+        int n =a.size();
+        int m =b.size();
+        int id=0;
+        int i =0;
+        int j =0;
+
+        int[] res = new int[n+m];
+
+        while(i<n && j<m){
+            if(a.get(i)<=b.get(j)){
+                res[id]=a.get(i);
+                id++;
+                i++;
+            }
+            else {
+                res[id]=b.get(j);
+                id++;
+                j++;
+            }
+        }
+        while(j<m){
+            res[id]=b.get(j);
+            id++;
+            j++;
+        }
+        while(i<n){
+            res[id]=a.get(i);
+            id++;
+            i++;
+        }
+        return res;
+    }
+}
