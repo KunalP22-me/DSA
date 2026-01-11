d3787e21f76c41604d9ae6fe100484a8e4851216
Kunal <pankunal3@gmail.com>
2026-01-11 22:53:16 +0530

    Update DAY_11.java

--- DAY_11.java
+++ DAY_11.java
@@ -26,3 +26,25 @@ class Solution {
         return true;
     }
 }
+
+
+
+//Problem 344 Leetcode
+
+class Solution {
+    public void reverseString(char[] s) {
+        int l=0;
+        int r=s.length-1;
+        while(l<r){
+            swap(s,l,r);
+            l++;
+            r--;
+    }
+}
+
+static void swap(char[] arr, int i, int j) {
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
