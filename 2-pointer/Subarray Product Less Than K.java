b88ad00d0b0322a21a4b5d9f9dce7fcdc05f6190
Kunal <pankunal3@gmail.com>
2026-01-13 22:04:55 +0530

    Create Subarray Product Less Than K.java

---
+++ 2-pointer/Subarray Product Less Than K.java
@@ -0,0 +1,24 @@
+// Problem 713 Leetcode
+
+class Solution { 
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        
+        if(k<=1) return 0;
+        
+        int c =0;
+        int i =0;
+        int p =1;
+        
+
+        for(int j =0; j<nums.length; j++){
+            p=p*nums[j];
+
+            while(p >= k){
+                p /=nums[i];
+                i++;
+            }
+            c=c+j-i+1;
+        }
+        return c;
+    }
+}
