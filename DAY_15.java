ee314279bd1dd56d9cc6fb10c53d0d96c211825a
Kunal <pankunal3@gmail.com>
2026-01-15 22:00:07 +0530

    Create DAY_15.java

---
+++ DAY_15.java
@@ -0,0 +1,39 @@
+//Problem 11 Leetcode
+
+class Solution {
+    public int maxArea(int[] height) {
+        int maxArea = 0;
+        int left = 0;
+        int right = height.length - 1;
+
+        while (left < right) {
+
+
+            //simple form
+            // maxArea = Math.max(maxArea, (right - left) * Math.min(height[left], height[right]));
+                        
+            int width = right - left;
+
+            int minHeight;
+            if (height[left] < height[right]) {
+                minHeight = height[left];
+            } else {
+                minHeight = height[right];
+            }
+
+            int area = width * minHeight;
+
+            if (area > maxArea) {
+                maxArea = area;
+            }
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;        
+    }
+}
