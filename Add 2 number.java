14f4d792ada4d22a12948122a539f13ae35f41bb
Kunal <pankunal3@gmail.com>
2026-02-03 23:41:21 +0530

    Update Add 2 number.java

--- Add 2 number.java
+++ Add 2 number.java
@@ -4,7 +4,7 @@ class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode head = new ListNode(0);
         ListNode l = head;
-        int c=0;
+        int c=0; 
 
         while(l1!=null || l2!=null || c!=0){
 
