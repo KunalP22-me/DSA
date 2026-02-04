f2b02315f7fe2f15c2b19881848d700bc7812607
Kunal <pankunal3@gmail.com>
2026-02-04 14:42:21 +0530

    Create DAY_32.java

---
+++ DAY_32.java
@@ -0,0 +1,21 @@
+//Problem 142 Leetcode
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode s=head;
+        ListNode f=head;
+
+        while(f!=null && f.next!=null){
+            s=s.next;
+            f=f.next.next;
+            if(s == f){
+                s=head;
+                while(s!=f){
+                    s=s.next;
+                    f=f.next;
+                }
+                return s;
+            }
+        }
+        return null;
+    }
+}
