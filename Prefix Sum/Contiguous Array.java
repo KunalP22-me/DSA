cb6b14c43284240455b70498e204c9b08061110a
Kunal <pankunal3@gmail.com>
2026-02-15 20:22:10 +0530

    Create Contiguous Array.java

---
+++ Prefix Sum/Contiguous Array.java
@@ -0,0 +1,32 @@
+//Problem 525 Leetcode
+import java.util.HashMap;
+
+class Solution {
+    public int findMaxLength(int[] nums) {
+
+        int n = nums.length;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int zero=0;
+        int one=0;
+        int res=0; 
+
+        for(int i=0; i<n; i++){
+            
+            if(nums[i]==0) zero++;
+            else one++;
+
+            int diff=zero-one;
+
+            if(diff==0) res=Math.max(res, i+1);
+
+            if(!map.containsKey(diff)){
+                map.put(diff,i);
+            } else{
+                int idx = map.get(diff);
+                int len = i - idx;
+                res = Math.max(res, len);
+            }
+        }
+        return res;
+    }
+}
