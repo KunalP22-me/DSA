13845cc89a78c0d9b7689027fe5fbfacb5de145d
Kunal <pankunal3@gmail.com>
2026-02-17 22:46:42 +0530

    Create DAY_48.java

---
+++ DAY_48.java
@@ -0,0 +1,27 @@
+//Problem 523 Leetcode
+
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int prefixSum = 0;
+
+        for(int i=0; i<nums.length; i++)
+        {
+            prefixSum += nums[i];
+            int rem = prefixSum % k;
+            if(rem < 0) rem += k;
+
+            if(map.containsKey(rem))
+            {
+                int prevIndex = map.get(rem);
+                if(i - prevIndex >= 2) return true;
+            }
+            else
+            {
+                map.put(rem, i);
+            }
+        }
+        return false;
+    }
+}
