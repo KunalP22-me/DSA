45aef4af5c7a12d2fb7eb666a167850eabae10e9
Kunal <pankunal3@gmail.com>
2026-02-20 00:16:46 +0530

    Create DAY_49.java

---
+++ DAY_49.java
@@ -0,0 +1,26 @@
+//Problem 1658 Leetcode
+
+class Solution {
+    public int minOperations(int[] nums, int x) {
+	
+    int sum = 0;
+	for (int num: nums) sum += num;
+
+	int maxLength = -1, currSum = 0;
+    int l=0;
+
+	for (int r=0; r<nums.length; r++) {
+		currSum += nums[r];
+
+		while (l <= r && currSum > sum - x){
+            currSum -= nums[l++];
+        }
+
+		if (currSum == sum - x) {
+            maxLength = Math.max(maxLength, r-l+1);
+        }
+	}
+
+	return maxLength == -1 ? -1 : nums.length - maxLength;
+    }
+}
