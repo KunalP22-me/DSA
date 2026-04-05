e7731cf04f9a74eae98e541e78c729d8fdb671fb
Kunal <pankunal3@gmail.com>
2026-04-05 23:19:31 +0530

    Create Missing Numbers.java

---
+++ Missing Numbers.java
@@ -0,0 +1,14 @@
+//Problem 268 Leetcode
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+
+        for(int i = 0; i < nums.length; i++)
+        {
+            n ^= i;
+            n ^= nums[i];
+        }
+
+        return n;
+    }
+}
