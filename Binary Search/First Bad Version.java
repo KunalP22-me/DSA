5408c9d0676ae867e08d274343daa998c79323ea
Kunal <pankunal3@gmail.com>
2026-04-07 22:22:24 +0530

    Improve firstBadVersion method implementation
    Refactor binary search logic for finding the first bad version.

--- Binary Search/First Bad Version.java
+++ Binary Search/First Bad Version.java
@@ -5,17 +5,16 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int l=0;
-        int h=n;
-        int res=0;
-        while(l<=h){
-            int m=(l+h)/2;
-            boolean is = isBadVersion(m);
-            if(is==false){
-                l=m+1;
-            }else{
-                h=m-1;
-                res=m;
+        int l = 0;
+        int h = n;
+        int res = 0;
+        while (l <= h) {
+            int m = l + (h - l) / 2;
+            if (isBadVersion(m)) {
+                h = m - 1;
+                res = m;
+            } else {
+                l = m + 1;
             }
         }
         return res;
