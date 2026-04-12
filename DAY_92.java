a4238495ece865894dcc3b0611d004a6946f59b4
Kunal <pankunal3@gmail.com>
2026-04-12 23:21:31 +0530

    Create DAY_92.java

---
+++ DAY_92.java
@@ -0,0 +1,16 @@
+// Problem 1413 Leetcode
+
+class Solution {
+    public int minStartValue(int[] nums) {
+        
+        int minVal = 0;
+        int prefixSum = 0;
+
+        for (int n : nums) {
+            prefixSum += n;
+            minVal = Math.min(minVal, prefixSum);
+        }
+
+        return 1 - minVal;
+    }
+}
