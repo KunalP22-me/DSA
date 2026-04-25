578a2debf82e8bffab0eb17aa7e008a533258264
Kunal <pankunal3@gmail.com>
2026-04-25 23:02:59 +0530

    Create DAY_97.java

---
+++ DAY_97.java
@@ -0,0 +1,24 @@
+//Problem 78 Leetcode
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> ss = new ArrayList<>();
+
+        fun(nums, 0, res, ss);
+        return res;
+    }
+
+    private void fun(int[] nums, int idx, List<List<Integer>> res, List<Integer> ss) {
+        if (idx == nums.length) {
+            res.add(new ArrayList<>(ss));
+            return;
+        }
+
+        ss.add(nums[idx]);
+        fun(nums, idx + 1, res, ss);
+
+        ss.remove(ss.size() - 1);
+        fun(nums, idx + 1, res, ss);
+
+    }
+}
