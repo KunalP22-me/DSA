8880b960221147a7dae0e0556ab2cf7bedc9379d
Kunal <pankunal3@gmail.com>
2026-05-08 23:25:12 +0530

    Create DAY_98.java

---
+++ DAY_98.java
@@ -0,0 +1,23 @@
+//GFG Palindrome string
+//only Recursion
+
+class Solution {
+    
+    boolean isPalindrome(String s) {
+        return check(s, 0, s.length() - 1);
+    }
+
+    boolean check(String s, int l, int h) {
+
+        // Base case
+        if(l >= h) return true;
+
+        // Mismatch
+        if(s.charAt(l) != s.charAt(h)) {
+            return false;
+        }
+
+        // Move inward
+        return check(s, l + 1, h - 1);
+    }
+}
