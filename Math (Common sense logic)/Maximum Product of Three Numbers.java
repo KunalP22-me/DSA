e83116b3c510792c97ed5fd7bbd56a5171df724f
Kunal <pankunal3@gmail.com>
2026-07-26 15:59:16 +0530

    Create Maximum Product of Three Numbers.java

---
+++ Math (Common sense logic)/Maximum Product of Three Numbers.java
@@ -0,0 +1,13 @@
+//Problem 628 Leetcode
+class Solution {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+
+        int n = nums.length-1;
+
+        return Math.max(
+            nums[0]*nums[1]*nums[n], // ✅ two smallest + largest buz (- * -) = +
+            nums[n-2]*nums[n-1]*nums[n]
+        );
+    }
+}
